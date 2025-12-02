26b885e55aaf3196a6abc09cc6ff6f398689c95d
Natugy <tanguy.sergeant@gmail.com>
2025-12-02 18:53:32 -0500

    Checkpoint from VS Code for coding agent session

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "TODO: fill in the docker build command"
+docker build -t project-flask -f Dockerfile.serving .
+sleep 10
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "TODO: fill in the docker run command"
+docker run -p 5000:5000 project-flask
+sleep 5
